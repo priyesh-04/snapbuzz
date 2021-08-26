e48c1ed99c53078d753c8e541d1f6de317261013
priyesh_04 <priyesh.shukla070@gmail.com>
2021-08-27 00:17:55 +0530

    Auto-generated commit [ci skip]

---
+++ assets/js/zuck-buildItem.js
@@ -0,0 +1,25 @@
+function buildItem(id, type, length, src, preview, link, seen, time) {
+
+    // Using object short-hand (id: id)
+
+    return {
+
+        id,
+
+        type,
+
+        length,
+
+        src,
+
+        preview,
+
+        link,
+
+        // seen,
+
+        time,
+
+    };
+
+}
