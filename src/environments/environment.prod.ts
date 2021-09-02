72f925bbe2b3f5dc65dd0a32b66d0b916af4b927
priyesh_04 <priyesh.shukla070@gmail.com>
2021-09-02 15:31:26 +0530

    bottom margin for posts and profile pages improved and search page search something box created to point search box at top

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // api_url: 'http://143.198.73.103/' // current_one
   api_url: 'https://snapbuzz-api.devverse.me/'
-  // api_url: 'http://localhost:8000/'
+  // api_url: "http://localhost:8000/",
 };
